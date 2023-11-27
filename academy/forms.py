6e9488cfa979da73345a54e9f3a3bfd6ebe23686
Manojdd10 <manojdd1029@gmail.com>
2023-11-27 12:12:07 +0530

    updated ui

--- academy/forms.py
+++ academy/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from accounts.models import CustomUser, FranchiseDetails, TeacherDetails, TeacherLevel
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+from crispy_forms.layout import Layout, Div, Field
 
 class TeacherLevelForm(forms.ModelForm):
     prev_level_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
@@ -13,14 +17,54 @@ class UserForm(forms.ModelForm):
         model = CustomUser
         fields = ['username', 'password', 'account_type']
 
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.helper = FormHelper(self)
+            self.helper.form_method = 'post'
+            self.helper.add_input(Submit('submit', 'Submit'))
+            self.helper.layout = Layout(
+            Div(
+                'username',
+                css_class='form-outline mb-4',
+                data_mdb_input_init=True
+            ),
+            Div(
+                'password',
+                css_class='form-outline mb-4',
+                data_mdb_input_init=True
+            ),
+            'account_type'
+        )
+
+      
+
+   
+
+        # self.fields['username'].widget.attrs.update({
+        #     'class': 'form-control',  # Add Bootstrap class
+        #     'placeholder': 'Enter your username',  # Placeholder text
+        #     'autocomplete': 'off',  # Disable autocomplete (optional)
+        #     # You can add other attributes as needed
+        # })
+
 class EditUserForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'password']
 
     def __init__(self, *args, **kwargs):
-       super(EditUserForm, self).__init__(*args, **kwargs)
-       self.fields['password'].initial = ""
+        super(EditUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'id': 'form1Example1',
+            'placeholder': 'Username',
+        })
+
+        self.fields['password'].widget = forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'id': 'form1Example2',
+            'placeholder': 'Password',
+        })
 
 class FranchiseDetailsForm(forms.ModelForm):
     class Meta:
